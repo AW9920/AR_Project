c56ce6e3cd74a454cc5faecd9e5f8de8eb67721e
Alexander Walder <alex.walder96@gmail.com>
2021-11-30 00:19:13 +0100

    Start Slingshot mechanism

---
+++ Assets/Scripts/GIZMO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GIZMO : MonoBehaviour
+{
+    public int GizSize = 3;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, GizSize);
+
+        Gizmos.DrawWireSphere(transform.position, GizSize + 0.1f);
+        Gizmos.color = Color.white;
+    }
+}
--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+
+    private bool isPressed;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPressed)
+        {
+            DragBall();
+        }
+    }
+
+    private void DragBall()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition.);
+        rb.position = mousePos;
+        Debug.Log(mousePos);
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
     }
 }
